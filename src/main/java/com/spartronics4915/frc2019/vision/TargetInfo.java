5743d75f3a12fe734192c9fdfeecd372903f1b9b
loqoman <darwin.s.clark@gmail.com>
2018-12-30 15:25:13 -0800

    Container to store values about vision readings. Needs to be renamed + refactored

--- src/main/java/com/spartronics4915/frc2019/vision/TargetInfo.java
+++ src/main/java/com/spartronics4915/frc2019/vision/TargetInfo.java
@@ -1,16 +1,20 @@
 package com.spartronics4915.frc2019.vision;
 
 /**
- * A container class for Targets detected by the vision system, containing the location in three-dimensional space.
+ * A container class for Targets detected by the vision system, containing the
+ * location in three-dimensional space, as well as their timestamps
  */
 public class TargetInfo {
     protected double x = 1.0;
     protected double y;
     protected double z;
 
-    public TargetInfo(double y, double z) {
+    double timestamp;
+
+    public TargetInfo(double y, double z, double timestamp) {
         this.y = y;
         this.z = z;
+        this.timestamp = timestamp;
     }
 
     public double getX() {
@@ -23,5 +27,9 @@ public class TargetInfo {
 
     public double getZ() {
         return z;
+    }
+
+    public double getTimestamp() {
+        return timestamp;
     }
 }
